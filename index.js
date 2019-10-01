92d38e3650b0e5560dd315274a12feb33370e737
Savo Debeljak <savo.debeljak96@gmail.com>
2019-10-01 14:28:58 +0200

    Zavrsi glavni dio weba. Dodaj username input i cuvanje rezultata odabira ispitanika. Napravi jos statistiku neku, moze to i konzola.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const path = require('path');
 const utilities = require('./public/js/custom/utilities');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', (req, res) => res.sendFile('./index.html', { root: __dirname }));
+app.get('/', (req, res) => res.sendFile('/index.html'));
 app.get('/images', utilities.getImages, (req, res) => res.json(req.images));
-app.post('/data', (request, response) => {
-    console.log(request.params);
+app.post('/data', (req, res) => {
+    fs.writeFileSync(path.join(__dirname, 'public', 'users', `${req.body.username}.json`), JSON.stringify(req.body));
+    res.sendFile('/thankyou.html', {
+        root: path.join(__dirname, 'public')
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
