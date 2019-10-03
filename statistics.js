649f468a4a9122bf7b46bd16b2456e92b6cd8188
Savo Debeljak <savo.debeljak96@gmail.com>
2019-10-03 15:56:07 +0200

    Dodaj funkcionalnost za prijasnji commit. Pokvari statistiku => TODO: popravi kuci.

--- statistics.js
+++ statistics.js
@@ -16,6 +16,12 @@ fs.readdir(path.join(__dirname, 'public', 'users'), (err, files) => {
                     } else {
                         dict[`${key}_CC`] += 1;
                     }
+                } else if(json[key].endsWith('_TT')) {
+                    if (dict[`${key}_TT`] == undefined) {
+                        dict[`${key}_TT`] = 1;
+                    } else {
+                        dict[`${key}_TT`] += 1;
+                    }
                 } else {
                     if (dict[key] == undefined) {
                         dict[key] = 1;
@@ -29,10 +35,13 @@ fs.readdir(path.join(__dirname, 'public', 'users'), (err, files) => {
     const keys = Object.keys(dict).sort();
     keys.forEach(key => {
         if (key.endsWith('_CC')) {
-            const percentage = (dict[key] / (dict[key] + dict[key.replace('_CC', '')])) * 100;
+            const percentage = (dict[key] / (dict[key] + dict[key.replace('_CC', '')] + dict[key.replace('_CC', '_TT')])) * 100;
             console.log(chalk.red.inverse(`${key.replace('radio-name-', '')}:`), chalk.red(starLength(dict[key])), chalk.red.inverse(`${percentage}%`));
+        } else if(key.endsWith('_TT')) {
+            const percentage = (dict[key] / (dict[key.replace('_TT', '')] + dict[key] + dict[`${key}_CC`])) * 100;
+            console.log(chalk.yellow.inverse(`${key.replace('radio-name-', '')}:`), chalk.yellow(starLength(dict[key])), chalk.yellow.inverse(`${percentage}%`));
         } else {
-            const percentage = (dict[key] / (dict[key] + dict[`${key}_CC`])) * 100;
+            const percentage = (dict[key] / (dict[key] + dict[`${key}_CC`] + dict[`${key}_TT`])) * 100;
             console.log(chalk.green.inverse(`${key.replace('radio-name-', '')}:`), chalk.green(starLength(dict[key])), chalk.green.inverse(`${percentage}%`));
         }
     })
