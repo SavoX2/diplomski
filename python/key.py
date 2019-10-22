c528e63afe60c8a9338b46e7e03ae269328d992f
Savo Debeljak <savo.debeljak96@gmail.com>
2019-10-22 19:50:16 +0200

    nez

--- python/key.py
+++ python/key.py
@@ -1,6 +1,9 @@
+import time
 from skimage.io import imread
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import skew
+from scipy.stats import kurtosis
 
 img = imread('./images/URChapel.jpg')
 
@@ -14,8 +17,6 @@ b = img[:, :, 2] / 255
 
 L = np.ravel(0.2126 * r + 0.7152 * g + 0.0722 * b)  # pretvori u 1D niz
 
-# print(img.shape, L.shape)
-
 # In this equation Lmax and Lmin are the maximum and minimum luminance values,
 # respectively, once a percentage of outlier pixels (both on the dark and bright sides) has been
 # eliminated.
@@ -24,8 +25,7 @@ L = np.ravel(0.2126 * r + 0.7152 * g + 0.0722 * b)  # pretvori u 1D niz
 
 L_sorted = np.sort(L)
 L_sorted_median = np.median(L_sorted)
-# print(L_sorted)
-# print(L_sorted_median, L_sorted[len(L)//2])
+
 
 def print_L_stats(L):
     print(len(L))
@@ -46,18 +46,48 @@ def print_L_stats(L):
     k = (logLH - logLmin) / (logLmax - logLmin)
     print('k =', k)
 
+    var = np.var(L)
+    print('V_L =', var)
+
+    std_dev = np.std(L)
+    print('Standardna devijacija =', std_dev)
+
+    skewness = skew(L)
+    print('Skewness =', skewness)
+
+    kurtosis_ = kurtosis(L)
+    print('Kurtosis =', kurtosis_)
+
+    temp = (L * 254) > 255
+    pov = np.sum(temp > 255)
+    print('pov =', pov)
+
+
 # print('Obicno L')
 # print_L_stats(L)
 # ovo je za slucaj da su outlieri po pola iznad i pola ispod mediana
-# print('\n\nL odsjeceno gore i dole za 2.5%')
-# print_L_stats([i for i in L_sorted[ np.int(len(L_sorted)*(2.5/100)) : np.int(len(L_sorted) - len(L_sorted)*(2.5/100)) ]])
-# print('\n\nL odsjeceno gore i dole za 0.5%')
-# print_L_stats([i for i in L_sorted[ np.int(len(L_sorted)*(0.5/100)) : np.int(len(L_sorted) - len(L_sorted)*(0.5/100)) ]])
+print('\n\nL odsjeceno gore i dole za 2.5%')
+print_L_stats([i for i in L_sorted[np.int(len(L_sorted)*(2.5/100))
+              : np.int(len(L_sorted) - len(L_sorted)*(2.5/100))]])
+print('\n\nL odsjeceno gore i dole za 0.5%')
+print_L_stats([i for i in L_sorted[np.int(len(L_sorted)*(0.5/100))
+              : np.int(len(L_sorted) - len(L_sorted)*(0.5/100))]])
 
-def mySort(x):
-    return L_sorted_median - x
+# apsolutna razlika medijana i vrijednosti niza
+L_diff_with_median = np.abs(L_sorted - np.median(L_sorted))
+# oni odzada ce biti najvise udaljeni od medijana
+sorted_indices = np.argsort(L_diff_with_median)
 
-# L_bla = L_sorted[]
+print('\n\nL odsjeceno po udaljenosti 5%', -
+      np.int(len(sorted_indices) * 5 / 100))
 
-print('Median', L_sorted_median)
-print(sorted(L_sorted, key=mySort)[-50:-1])
+# posljednih 5% niza dobijenog argsort-om su oni najudaljeniji od medijana - outlier-i
+indices_to_delete = sorted_indices[-np.int(len(sorted_indices) * 5 / 100):]
+print_L_stats(np.delete(L_sorted, indices_to_delete))
+
+print('\n\nL odsjeceno po udaljenosti 1%', -
+      np.int(len(sorted_indices) * 1 / 100))
+
+# posljednih 1% niza dobijenog argsort-om su oni najudaljeniji od medijana - outlier-i
+indices_to_delete = sorted_indices[-np.int(len(sorted_indices) * 1 / 100):]
+print_L_stats(np.delete(L_sorted, indices_to_delete))
